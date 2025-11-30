d27e32bbf08c9c4f51aa954efc30c609ddcb1f23
Adam Uklański <48825965+Iluzjonista@users.noreply.github.com>
2025-11-30 23:50:42 +0100

    added api connection

---
+++ src/utils/api.ts
@@ -0,0 +1,26 @@
+import { Debt } from './types';
+
+const BASEURL = 'https://rekrutacja-webhosting-it.krd.pl/api/Recruitment';
+
+export async function GetTopDebts(): Promise<Debt[]> {
+    const response = await fetch(`${BASEURL}/GetTopDebts`);
+    if (!response.ok) throw new Error(`Network response was not ok, HTTP ${response.status}`);
+    return response.json();
+};
+
+export async function GetFilteredDebts(phrase: string): Promise<Debt[]> {
+    const response = await fetch(`${BASEURL}/GetFilteredDebts`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ phrase })
+    });
+    if (response.status === 405) throw new Error('Phrase too short');
+    if (!response.ok) throw new Error(`Network response was not ok, HTTP ${response.status}`);
+    return response.json();
+}
+
+export async function GetDebtsCount(): Promise<Debt[]> {
+    const response = await fetch(`${BASEURL}/GetDebtsCount`);
+    if (!response.ok) throw new Error(`Network response was not ok, HTTP ${response.status}`);
+    return response.json();
+};
