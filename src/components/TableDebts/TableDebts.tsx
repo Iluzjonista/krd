ecb92eca647702221e80c55fc116d89f0c5f23f8
Adam Uklański <48825965+Iluzjonista@users.noreply.github.com>
2025-12-01 15:52:43 +0100

    Added loading data to table

--- src/components/TableDebts/TableDebts.tsx
+++ src/components/TableDebts/TableDebts.tsx
@@ -1,11 +1,36 @@
+import { Debt } from '../../utils/types';
 import styles from './tableDebts.module.scss';
 
-export default function TableDebts() {
+type TableDebtsProps = {
+  data: Debt[];
+  
+};
+
+
+export default function TableDebts({ data }: TableDebtsProps) {
 
   return (
     <div className={styles.container}>
+        
       <table className={styles.tableDebts}>
-        test
+        <thead>
+          <tr>
+            <th>Dłużnik</th>
+            <th>NIP</th>
+            <th>Kwota zadłużeniaValue</th>
+            <th>Data powstałego zobowiązania</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((debt) => (
+            <tr key={debt.Id}>
+              <td>{debt.Name}</td>
+              <td>{debt.NIP}</td>
+              <td>{debt.Value}</td>
+              <td>{debt.Date}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
--- src/pages/TableDebtPage.tsx
+++ src/pages/TableDebtPage.tsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from "react";
 import Loader from "../components/Loader/Loader";
 import SearchPanel from "../components/SearchPanel/SearchPanel";
 import TableDebts from "../components/TableDebts/TableDebts";
+import { Debt } from "../utils/types";
+import { GetTopDebts } from "../utils/api";
 import styles from "./tableDebtPage.module.scss";
 
 export default function TableDebtPage() {
+    const [debts, setDebts] = useState<Debt[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const [top] = await Promise.all([GetTopDebts()]);
+                setDebts(top);
+            } catch {
+                console.log('Nie udało się załadować danych.');
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, []);
+
     return (
         <div className={styles.debtPage}>
             <SearchPanel />
             <div className={styles.tableContainer}>
-                <Loader />
-                <TableDebts />
+                {loading ? <Loader /> : <TableDebts data={debts} />}
             </div>
         </div>
     );
