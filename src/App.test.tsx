c0228cd81f8f9bcfe47e573705d12f9fc1016fd5
Adam Uklański <48825965+Iluzjonista@users.noreply.github.com>
2025-12-03 11:13:41 +0100

    create sample test names

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders input and search button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+test('Renders table', () => {
+  render(<App />);
+});
+test('Renders filtering', () => {
+  render(<App />);
+});
+test('Renders Error message - too short input', () => {
+  render(<App />);
+});
+test('Renders loader', () => {
+  render(<App />);
+});
+test('Sorts data by NIP ascending', () => {
+  render(<App />);
+});
+test('Show correct date', () => {
+  render(<App />);
 });
